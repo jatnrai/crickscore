702d12939cc855306c52afcda3a0c35f85c443e5
jatnrai <raijatin62@gmail.com>
2024-07-05 18:09:43 +0530

    commit01

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Components/Home/Home';
+import LiveMatch from './Components/LiveMatch/LiveMatch';
+import Login from './Components/Auth/Login/Login';
+import SignIn from './Components/Auth/Signin/SignIn';
 
 function App() {
   return (
-    <div className="App">
-    <h1>hello</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/livematch" element={<LiveMatch />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signin" element={<SignIn />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Components/LiveMatch/LiveMatch.js
@@ -0,0 +1,8 @@
+// LiveMatch.js
+import React from 'react';
+
+function LiveMatch() {
+  return <h2>Live Match</h2>;
+}
+
+export default LiveMatch;
